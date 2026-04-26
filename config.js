6129357faa61b49ab39c8d75878749062aea9b3e
javierfernandez888-debug <javierfernandez888@gmail.com>
2026-04-26 23:01:16 +0200

    Refactor order page to use centralized app config

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  menuItems: [
+    { id: 'muzza', name: 'Muzza', price: 6700 },
+    { id: 'jamon', name: 'Jamón y Morrón', price: 7700 },
+    { id: 'roquefort', name: 'Roquefort', price: 7700 }
+  ],
+  delivery: {
+    label: 'Dirección de entrega:',
+    placeholder: 'Ej: San Martín 123'
+  },
+  whatsapp: {
+    phone: '543444497985',
+    display: '+54 3444 49-7985'
+  }
+};
